33cc20268bc1a2a16aac070d635619a0a9facef3
James Louie Puro <james.louie.puro@gmail.com>
2010-09-05 19:49:24 -0400

    Remove log file

--- application/logs/2010/09/05.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2010-09-05 18:36:44 --- ERROR: ReflectionException [ -1 ]: Class controller_ninjas-corner does not exist ~ SYSPATH/classes/kohana/request.php [ 1028 ]
